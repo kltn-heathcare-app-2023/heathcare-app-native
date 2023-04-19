09ec70b202bf104aa97a12e310d0327c17446181
iamtun <letuan19431791.iuh@gmail.com>
2023-04-19 23:03:05 +0700

    create ui forgot pass

--- components/ActionView/index.js
+++ components/ActionView/index.js
@@ -1,29 +1,33 @@
-import { StyleSheet, View, Text } from "react-native";
+import {StyleSheet, View, Text} from 'react-native';
 
-function ActionView({ title, isLogin, handle }) {
-    return (
-        <View style={styles.actionView}>
-            <Text style={styles.action} onPress={handle}>
-                {title}
-            </Text>
-            {isLogin && <Text style={styles.action}>Quên mật khẩu</Text>}
-        </View>
-    );
+function ActionView({title, isLogin, handle, handleB}) {
+  return (
+    <View style={styles.actionView}>
+      <Text style={styles.action} onPress={handle}>
+        {title}
+      </Text>
+      {isLogin && (
+        <Text style={styles.action} onPress={handleB}>
+          Quên mật khẩu
+        </Text>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    actionView: {
-        marginTop: 16,
-        width: "90%",
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    action: {
-        fontSize: 16,
-        textDecorationLine: "underline",
-        color: "#252323",
-        fontWeight: "600",
-    },
+  actionView: {
+    marginTop: 16,
+    width: '90%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  action: {
+    fontSize: 16,
+    textDecorationLine: 'underline',
+    color: '#252323',
+    fontWeight: '600',
+  },
 });
 
 export default ActionView;
